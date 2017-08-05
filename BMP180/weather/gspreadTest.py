14aedd990e2ac4fbe97b0b02ee347857ce8d7883
Ron Lee <ronwlee@gmail.com>
2017-08-04 20:26:36 -0700

    add weather directory

---
+++ BMP180/weather/gspreadTest.py
@@ -0,0 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('Raspberry Pi Weather Station-6831df99334f.json', scope)
+
+gc = gspread.authorize(credentials)
+
+wks = gc.open("rpiSensorLog").sheet1
